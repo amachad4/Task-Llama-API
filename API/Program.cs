7a9c53d4087961e4d16c99bca66f08643bfafc7d
amachad4 <amachad4@masonlive.gmu.edu>
2023-05-10 04:09:27 -0400

    Fix cors issue

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
+
 app.UseMiddleware<ExceptionMiddleWare>();
 
 if (app.Environment.IsDevelopment())
